1ddf003d033cf8489358e093da9e0428dec870b9
Juhstin <jefu@uci.edu>
2020-02-16 11:24:01 -0800

    Add files via upload

---
+++ Proj2.py
@@ -0,0 +1,161 @@
+import sys
+
+D = []
+OFT = []
+
+
+def init():
+    D.clear()
+    OFT.clear()
+
+    for i in range(64): # D[64][512]
+        D.append([''] * 512)
+
+    for i in range(1,7): # D[1] to D[6] are fd blocks
+        for j in range(0,509,4):
+            D[i][j] = -1
+
+    D[1][0] = 0 # fd 0 is initially length 0
+    D[1][1] = 7 # fd 0 initally has block 7 allocated
+
+    for i in range(len(D[0])): # D[0] to D[7] allocated, D[8] to D[63] are free
+        if i >= 0 and i <= 7:
+            D[0][i] = 1
+        else:
+            D[0][i] = 0
+
+    for i in range(len(D[7])):
+        D[7][i] = '0' # CHANGED HERE from '0'
+
+    for i in range(0,4):
+        if i == 0:
+            OFT.append({
+                "RW": "1",
+                "POSITION": -1,
+                "FSIZE": -1,
+                "FDINDEX": -1
+            })
+        else:
+            OFT.append({
+                "RW": "1",
+                "POSITION": 0,
+                "FSIZE": 0,
+                "FDINDEX": 0
+            })
+
+    print("System Initialized")
+
+def create(name):
+    global freeBlockIndex
+
+    if len(name) > 3:
+        print("Error")
+        return
+
+    # See if file already exists
+    if fileExists(name):
+        print("Error: duplicate file name")
+        return
+
+    # Search for free file descriptor
+    fdBlock, fdIndex = findFreeFD()
+    if fdBlock == -1 and fdIndex == -1:
+        print("Error: too many files")
+        return
+
+    # If size of file is 512 or 1024, need to create a new block
+    if D[1][0] == 512 or D[1][0] == 1024:
+        newBIndex = getNewBlock()
+        if newBIndex != -1:
+            if assignNewBlock(1,0,newBIndex) == False:
+                print("Error: file out of space")
+                return
+            for i in range(len(D[newBIndex])):
+                D[newBIndex][i] = '0'
+            #Need to add to OFT HERE
+
+    # Search for free entry in directory
+    # Seek D[7] to 0
+    dirList = list((x for x in D[1][1:4] if type(x) == int))
+    for dir in dirList: # Get list of blocks directory has
+        for i in range(0,505,8): # Need to change this later to start on the position in OFT
+            if D[dir][i] != '0' and i == 504 and len(dirList) == 3 and dir == dirList[-1]: #If last 8 bytes of 3rd block
+                print("Error: no free directory entry found")
+                return
+
+            if D[dir][i] == '0': # Space in directory
+                # Assigning descriptor to file
+                D[fdBlock][fdIndex] = 0  # -1 to 0 to indicate new file is empty
+
+                nameList = list(name)
+                for j in range(0, 4 - len(nameList)): # Append end of string char if name is < 4 chars
+                    nameList.append('/')
+                nameList.append(fdIndex)
+                for j in range(0,3):
+                    nameList.append('')
+
+                for k,letter in enumerate(nameList,i): # Overwriting
+                    D[dir][k] = letter
+                break
+
+    D[1][0] += 8 # fd[0] (directory file) size is now 8 more bytes
+    print("Success: file",name, "created")
+
+def delete(name):
+    pass
+
+def seek(i,p):
+    pass
+
+def open(name):
+    pass
+    #
+    # if len(freeBlockList) == 0:
+    #     D[fdBlock][fdIndex].append(freeBlockIndex)
+    #     freeBlockIndex += 1
+    # else:
+    #     D[fdBlock][fdIndex].append(freeBlockList[0])
+    #     freeBlockList.pop(0);
+
+# HELPER FUNCTIONS
+def findFreeFD():
+    for i in range(1,7):
+        for j in range(0,509,4):
+            if D[i][j] == -1:
+                return i,j
+    return -1,-1
+
+def fileExists(name):
+    # Seek D[7] to 0
+    for i in range(0,505, 8):
+        fName = ""
+        fName = fName.join(D[7][i:i+4])
+        if fName == name:
+            return True
+    return False
+
+def getNewBlock():
+    for i in range(len(D[0])):
+        if D[0][i] == 0:
+            return i
+    return -1
+
+def assignNewBlock(fdBlock,fdIndex,newBIndex):
+    for i in range(fdIndex+1,fdIndex+4):
+        if D[fdBlock][i] == '':
+            D[fdBlock][i] = newBIndex
+            return True;
+    return False;
+
+
+
+if __name__ == "__main__":
+    init()
+    create("Tes")
+    create("Te")
+
+    for i,row in enumerate(D,0):
+        print("D[" + str(i) + "]:",row)
+
+
+
